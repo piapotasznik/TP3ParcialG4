a6f96a2acca0e088001d73095461ff17ca43e672
piapotasznik <piapotasznik@gmail.com>
2023-11-02 21:50:59 -0300

    Initial commit

---
+++ app/src/main/java/edu/ort/tp3parcialg4/Activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.ort.tp3parcialg4.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.ort.tp3parcialg4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TP3ParcialG4"
+include(":app")
+ 
